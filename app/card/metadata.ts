07163b543ca96cfc7dd7de9e2dd879c394fc5ba0
SillyHippy <159052837+SillyHippy@users.noreply.github.com>
2025-04-07 17:49:51 -0500

    Update metadata.ts

--- app/card/metadata.ts
+++ app/card/metadata.ts
@@ -1,10 +1,10 @@
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Digital Business Card',
-  description: 'Connect with Joseph Iannazzi at Just Legal Solutions. Download contact details, schedule reminders, and get in touch easily.',
+  title: 'Digital Business/Resume(s) Card',
+  description: 'Connect with Joseph Iannazzi.',
   openGraph: {
-    title: 'Digital Business Card | Just Legal Solutions',
-    description: 'Connect with Joseph Iannazzi at Just Legal Solutions. Download contact details, schedule reminders, and get in touch easily.'
+    title: 'Digital Business/Resume(s) Cards',
+    description: 'Connect with Joseph Iannazzi.'
   }
 };
