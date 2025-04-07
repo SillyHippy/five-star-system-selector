690d2d5eeea3fb9f0997268bd769dc4930b26e64
SillyHippy <159052837+SillyHippy@users.noreply.github.com>
2025-04-07 17:44:19 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Joseph Iannazzi | Digital Business Card',
-  description: 'Professional digital business card for Joseph Iannazzi.',
+  title: 'Joseph Iannazzi | Digital Business/Resume(s) Card',
+  description: 'Digital Business/Resume(s) Card for Joseph Iannazzi.',
 };
 
 export default function RootLayout({
